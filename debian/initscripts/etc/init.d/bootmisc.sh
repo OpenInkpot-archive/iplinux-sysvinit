9ac0952689193c67312df802c1828e7aef327d79
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-12-31 14:30:19 +0100

    Clean up initscripts

--- debian/initscripts/etc/init.d/bootmisc.sh
+++ debian/initscripts/etc/init.d/bootmisc.sh
@@ -1,81 +1,8 @@
-#
-# bootmisc.sh	Miscellaneous things to be done during bootup.
-#
-# Version:	@(#)bootmisc.sh  2.85-17  04-Jun-2004  miquels@cistron.nl
-#
+# -*- mode: sh -*-
 
-DELAYLOGIN=yes
-VERBOSE=yes
-EDITMOTD=yes
-[ -f /etc/default/rcS ] && . /etc/default/rcS
-
-#
-#	Put a nologin file in /etc to prevent people from logging in
-#	before system startup is complete.
-#
-if [ "$DELAYLOGIN" = yes ]
-then
-	echo "System bootup in progress - please wait" > /etc/nologin
-fi
-
-#
-#	Create /var/run/utmp so we can login.
-#
 : > /var/run/utmp
 if grep -q ^utmp: /etc/group
 then
 	chmod 664 /var/run/utmp
 	chgrp utmp /var/run/utmp
 fi
-
-#
-#	Set pseudo-terminal access permissions.
-#
-if [ ! -e /dev/.devfsd ] && [ -c /dev/ttyp0 ]
-then
-	chmod 666 /dev/tty[p-za-e][0-9a-f] > /dev/null 2>&1
-	chown root:tty /dev/tty[p-za-e][0-9a-f] > /dev/null 2>&1
-fi
-
-#
-#	Update /etc/motd. If it's a symbolic link, do the actual work
-#	in the directory the link points to.
-#
-if [ "$EDITMOTD" != no -a -f /etc/motd ]
-then
-    if [ -L /etc/motd ]
-    then
-        MOTD="`readlink /etc/motd`"
-    else
-        MOTD="/etc/motd"
-    fi
-	if [ "$MOTD" != "" ]
-	then
-		uname -a > $MOTD.tmp
-		sed 1d $MOTD >> $MOTD.tmp
-		mv $MOTD.tmp $MOTD
-	fi
-fi
-
-#
-#	Save kernel messages in /var/log/dmesg
-#
-if [ -x /bin/dmesg ] || [ -x /sbin/dmesg ]
-then
-	dmesg -s 524288 > /var/log/dmesg
-elif [ -c /dev/klog ]
-then
-	dd if=/dev/klog of=/var/log/dmesg &
-	dmesg_pid=$!
-	sleep 1
-	kill $dmesg_pid
-fi
-
-
-#
-#	Remove ".clean" files.
-#
-rm -f /tmp/.clean /var/run/.clean /var/lock/.clean
-
-: exit 0
-
--- debian/initscripts/etc/init.d/hostname.sh
+++ debian/initscripts/etc/init.d/hostname.sh
@@ -1,11 +1,6 @@
-#
-# hostname.sh	Set hostname.
-#
-# Version:	@(#)hostname.sh  1.10  26-Feb-2001  miquels@cistron.nl
-#
+# -*- mode: sh -*-
 
 if [ -f /etc/hostname ]
 then
 	hostname -F /etc/hostname
 fi
-
